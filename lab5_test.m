9546a57bea181325c2f01600ee1ec5211208e475
Adil Tobaa <aatobaa@uchicago.edu>
2016-05-03 23:56:46 -0500

    leaving some todos for tomorrow

--- lab5_test.m
+++ lab5_test.m
@@ -2,6 +2,7 @@
 
 SAMPLING_RATE = 22050;
 
+% Load the data
 kira1 = wavread('Kira_Training1.wav');
 kira2 = wavread('Kira_Training2.wav');
 kira3 = wavread('Kira_Training3.wav');
@@ -9,7 +10,8 @@ pascal1 = wavread('Pascal_Training1.wav');
 pascal2 = wavread('Pascal_Training2.wav');
 pascal3 = wavread('Pascal_Training3.wav');
 %%
-
+% Extract relevant features, which are basically frequency bands averaged
+% over time. 
 kira1_c = extract_features(kira1);
 kira2_c = extract_features(kira2);
 kira3_c = extract_features(kira3);
@@ -22,6 +24,7 @@ male_train = [pascal1_c pascal2_c pascal3_c];
 
 trainingData = [female_train male_train];
 
+%Just counting the number of inputs broken out of each sound wave. 
 k1s = size(kira1_c,2);
 k2s = size(kira2_c,2);
 k3s = size(kira3_c,2);
@@ -32,6 +35,7 @@ p3s = size(pascal3_c,2);
 % 1 represents female, 0 represents male
 target = [repmat(1,k1s+k2s+k3s,1)' repmat(0,p1s+p2s+p3s,1)'];
 
+% Normalizing the data between -1 and 1 
 minVoice = min(trainingData');
 maxVoice = max(trainingData');
 
@@ -50,9 +54,9 @@ end
 %%
 trainingData = [normFemale normMale];
 
-%%
+%% Creating the Neural Network - Backpropagation learning rule
 
-NHIDDEN = 10;
+NHIDDEN = 10; 
 NINP = NUM_FEATURES;
 NOUT = 1;
 
@@ -63,13 +67,16 @@ bo = zeros(NOUT,1); %bias weights feeding output nodes
 lr = 0.1;
 transfer_fn=@(x,alpha) 1./(1+exp(alpha*x));
 
+%variables are named as "inp_input" meaning input to the input layer,
+%"inp_hidden" meaning input to the hidden layer, "net_input_h" meaning net
+%input to the hidden layer, etc. 
 inp_input = trainingData;
 
-%Derivations below assume sigmoid transfer fxn with alpha of -1;
 numEpochs = 1000;
 allErrors = [];
 
 numTrainingInput = size(inp_input,2);
+%Transfer function derivative below assumes sigmoid transfer fxn with alpha of -1;
 
 for j = 1:numEpochs
     epochError = 0;
@@ -98,6 +105,8 @@ for j = 1:numEpochs
         bh = bh + lr*transfer_fn(net_input_h,-1).*(1-transfer_fn(net_input_h,-1))*sum((Wo*errors_o));
         
     end
+    
+    % Calculate the error from this epoch. 
     for k = 1:numTrainingInput
         net_input_h = Wh*inp_input(:,k) + bh;
         output_h = transfer_fn(net_input_h,-1);
@@ -106,16 +115,30 @@ for j = 1:numEpochs
         output_o = transfer_fn(net_input_o,-1);
         epochError = epochError + (target(k)-output_o)^2;
     end
-    epochError
     allErrors = [allErrors; epochError];
 end
-%%
+%% Visualize classification 
+% Feature Classification
 net_input_h = Wh*trainingData + repmat(bh,1,numTrainingInput);
 output_h = transfer_fn(net_input_h,-1);
 inp_hidden = output_h; 
 net_input_o = Wo*inp_hidden + bo; 
-bpGreeblesClassification = transfer_fn(net_input_o,-1);
-plot(bpGreeblesClassification,'.')
+voiceClassification = transfer_fn(net_input_o,-1);
+plot(voiceClassification,'.')
+% Average back features per sound bite
+
+%TODO: Give a final classification of each sound bite
+%TODO: Make plots to show that it classifies reliably. 
+
+voiceClassification(1:k1s)
+voiceClassification(1:k1s)
+voiceClassification(1:k1s)
+
+voiceClassification(1:p1s)
+voiceClassification(1:p1s)
+voiceClassification(1:p1s)
+
+finalClassification = [mean(voiceClassification(1:k1s)) mean(voiceClassification(1+k1s:)) mean(voiceClassification(1:k1s)) mean(voiceClassification(1:k1s)) mean(voiceClassification(1:k1s)) mean(voiceClassification(1:k1s))]
 
 
 
